d1b7fb6fc4574f774abee679400b4a9c3674c8c0
JacobHu0723 <63916353+JacobHu0723@users.noreply.github.com>
2022-04-07 15:55:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,58 +14,58 @@ while 1:
     try:
         a=int(input("Please select type:(1.Arrrangement 2.Combination)\n(Exit program with other characters)"))
     except:
-        print("Illegal input!")
+        print("\nIllegal input!")
         break
     else:
         if a==1:
-            print("For example：A^m_n")
+            print("For example：","$A^m_n$")
             try:
                 m=int(input("m:"))
             except:
-                print("Illegal input!")
+                print("Illegal input!\n")
             else:
                 if m<=0:
-                    print("It must be greater than 0!")
+                    print("It must be greater than 0!\n")
                 else:
                     try:
                         n=int(input("n:"))
                     except:
-                        print("Illegal input!")
+                        print("Illegal input!\n")
                     else:
                         if n<=0:
-                            print("It must be greater than 0!")
+                            print("It must be greater than 0!\n")
                         elif m>n:
-                            print("m mustn't be greater than n!")
+                            print("m mustn't be greater than n!\n")
                         else:
                             ans=arrangement(m,n)
                             print("The result is:",ans)
             
         elif a==2:
             c_ans=1
-            print("For example：C^m_n")
+            print("For example：","$C^m_n$")
             try:
                 m=int(input("m:"))
             except:
-                print("Illegal input!")
+                print("Illegal input!\n")
             else:
                 if m<=0:
-                    print("It must be greater than 0!")
+                    print("It must be greater than 0!\n")
                 else:
                     try:
                         n=int(input("n:"))
                     except:
-                        print("Illegal input!")
+                        print("Illegal input!\n")
                     else:
                         if n<=0:
-                            print("It must be greater than 0!")
+                            print("It must be greater than 0!\n")
                         elif m>n:
-                            print("m mustn't be greater than n!")
+                            print("m mustn't be greater than n!\n")
                         else:
                             ans=combination()
                             print("The result is:",ans)
             
         else:
-            print("Illegal input!")
+            print("\nIllegal input!\n\n")
             continue
         
 print("Thanks for using!")
